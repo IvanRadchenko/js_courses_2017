81e303c5517a5b9c4c5478841bdb3d858f586427
Ivan Radchenko <radchenk@gmail.com>
2017-01-28 11:01:55 +0300

    #5 - Fix formula of fibBinet

--- Bine/fibBinet.js
+++ Bine/fibBinet.js
@@ -8,7 +8,7 @@
 
 function fibBinet(n) {
   var phi = (1 + Math.sqrt(5)) / 2;
-  return Math.round(Math.pow(phu, n)) / Math.sqrt(5);
+  return Math.round(Math.pow(phi, n)) / Math.sqrt(5);
 }
 
 fibBinet();
